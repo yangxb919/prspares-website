a749168f7e94fee66d143752c77bd319c966b75a
bowen yang <84960318+yangxb919@users.noreply.github.com>
2025-10-17 23:25:34 +0800

    Fix: Add external image hosting domains to Next.js config

--- next.config.js
+++ next.config.js
@@ -28,6 +28,43 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      // 添加常见的图床域名支持
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'imgur.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
